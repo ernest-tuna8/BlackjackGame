c23e2af1e71a0fc0a6fb751e3f912aaba1b88bbe
clc babischkin <n/a>
2020-12-09 14:23:51 -0600

    made label outlets and started initial variables

--- BlackjackGame/ViewController.swift
+++ BlackjackGame/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var DealerLabel: UILabel!
+    @IBOutlet weak var Playerlabel: UILabel!
+    let usercard1: Int = -1
+    let usercard2: Int = -1
+    let Dealercard1: Int = -1
+    let Dealercard2: Int = -1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        usercard1 = Card.generateCard(self:Card)
+        usercard2 = Card.generateCard(self: Card)
+        
     }
 
 
